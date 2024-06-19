a3ec043548e63ff892a76a1890848d1b1e7b391e
Elysson MR <elyssonmr@gmail.com>
2024-06-18 22:52:03 -0300

    Add html page (second class)

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -6,3 +7,20 @@ app = FastAPI()
 @app.get('/')
 def hello():
     return {'message': 'Olá Mundo!'}
+
+
+html_content = """
+<html>
+    <head>
+        <title>Olá Mundo!!</title>
+    </head>
+    <body>
+        <h1>Olá Mundo!!</h1>
+    </body>
+</html>
+"""
+
+
+@app.get('/hello_text', response_class=HTMLResponse)
+def hello_text():
+    return HTMLResponse(html_content)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,26 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
-from fast_zero.app import app
+from fast_zero.app import app, html_content
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+@pytest.fixture()
+def client():
+    return TestClient(app)
 
+
+def test_should_return_helloworld_json(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_should_return_helloworld_html(client):
+    response = client.get('/hello_text')
+
+    assert response.status_code == HTTPStatus.OK
+    assert 'Olá Mundo!!' in response.text
+    assert response.text == html_content
